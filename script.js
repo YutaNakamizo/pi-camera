7ccfcd7f791e9b98d269e5de6e312a302ad31451
Yuta Nakamizo <yutanakamizo@Yutas-MacBook-Pro.local>
2019-09-15 00:33:36 +0900

    only preview and few UI.

--- script.js
+++ script.js
@@ -1,17 +1,56 @@
 console.log("script loading...");
 
 
-const setPreview = (camera)=>{
+/* Media Stream */
+const getStream = ()=>{
+  return navigator.mediaDevices.getUserMedia({
+    video: true,
+    //使うカメラをインカメラか背面カメラかを指定する場合には
+    //video: { facingMode: "environment" },//背面カメラ
+    //video: { facingMode: "user" },//インカメラ
+    audio: true
+  });
+};
+
+
+
+
+/* Preview Area */
+const setPreview = (stream)=>{
+  const screen = document.getElementById("preview");
+  screen.srcObject = stream;
+};
+
+
+
+/* Video Recording */
+const getRecorder = stream=>{
+  const recorder = new MediaRecorder(stream);
   
+  recorder.ondataavailable = e=>{};
   
+  recorder.onstop = ()=>{};
+
+
+
+  return recorder;
 };
 
 
 
 
 
-document.addEventListener("DOMContentLoaded",()=>{
+
+
+document.addEventListener("DOMContentLoaded",async ()=>{
+  const stream = await getStream();
   
+  console.log(stream);
+  
+
+  setPreview(stream);
+  
+  const recorder = getRecorder();
 });
 
 
